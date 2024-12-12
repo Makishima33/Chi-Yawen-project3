b677e06ee1798f1abbda783f4de05535b0304f2d
Yawen Chi <lovemakishimashogo@gmail.com>
2024-12-11 21:26:07 -0800

    update config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/",
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "https://chi-yawen-project3-backend.onrender.com",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+});
